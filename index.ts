68fb779a191ba7744eec560546e9d9b4e771cfd5
Milena Pieńkosz <designbymilencja@gmail.com>
2023-03-05 11:24:30 +0000

    added rate-limit.

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import 'express-async-errors';
+import rateLimit from 'express-rate-limit';
 import {handleError} from "./utils/errors";
 const app = express();
 
@@ -8,7 +9,11 @@ app.use(cors({
     origin: 'http://localhost:3000',
 }));
 app.use(express.json());
-
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+});
+app.use(limiter);
 app.use(handleError);
 
 app.listen(3001, '0.0.0.0', ()=> {
