cac473d7df7cd550fdf6dbaf94a82494d4c84dc0
Milena Pieńkosz <designbymilencja@gmail.com>
2023-03-15 15:33:43 +0000

    Added all search method and changed for country record

--- records/payment.record.ts
+++ records/payment.record.ts
@@ -13,15 +13,40 @@ export class PaymentRecord implements PaymentEntity {
     boughtAt: string;
     idCategory: string;
     idPlace: string;
+    sumOneCategory: number;
+    sumOneCategoryInOneCountry: number;
+    sumAllCategoriesInOneCountry: number;
+    sumOneCategoryInOneMonth: number;
+    sumAllCategoriesInOneMonth: number;
+    sumInYear: number;
 
     constructor(obj: PaymentEntity) {
-        const {id, cost, currency, boughtAt, idCategory, idPlace} = obj;
+        const {
+            id,
+            cost,
+            currency,
+            boughtAt,
+            idCategory,
+            idPlace,
+            sumOneCategory,
+            sumOneCategoryInOneCountry,
+            sumAllCategoriesInOneCountry,
+            sumOneCategoryInOneMonth,
+            sumAllCategoriesInOneMonth,
+            sumInYear,
+        } = obj;
         this.id = id;
         this.cost = cost;
         this.currency = currency;
         this.boughtAt = boughtAt;
         this.idCategory = idCategory;
         this.idPlace = idPlace;
+        this.sumOneCategory = sumOneCategory;
+        this.sumOneCategoryInOneCountry = sumOneCategoryInOneCountry;
+        this.sumAllCategoriesInOneCountry = sumAllCategoriesInOneCountry;
+        this.sumOneCategoryInOneMonth = sumOneCategoryInOneMonth;
+        this.sumAllCategoriesInOneMonth = sumAllCategoriesInOneMonth;
+        this.sumInYear = sumInYear;
 
         if (!cost || cost === 0) {
             throw new ValidationError('Musisz podać koszt , aby dodać Twoją płatność')
@@ -46,21 +71,21 @@ export class PaymentRecord implements PaymentEntity {
         }
     }
 
-
     async insertPayment(): Promise<string> {
         if (!this.id) {
             this.id = uuid();
         } else {
             throw new Error('Cannot insert sth that is already added!');
         }
-        await pool.execute('INSERT INTO `payments` VALUES (:id, :cost, :currency, :boughtAt, :idPlace, :idCategory)', {
-            id: this.id,
-            cost: this.cost,
-            currency: this.currency,
-            boughtAt: this.boughtAt,
-            idPlace: this.idPlace,
-            idCategory: this.idCategory,
-        })
+        await pool.execute('INSERT INTO `payments` VALUES (:id, :cost, :currency, :boughtAt, :idPlace, :idCategory)',
+            {
+                id: this.id,
+                cost: this.cost,
+                currency: this.currency,
+                boughtAt: this.boughtAt,
+                idPlace: this.idPlace,
+                idCategory: this.idCategory,
+            })
         return this.id;
     };
 
@@ -71,65 +96,56 @@ export class PaymentRecord implements PaymentEntity {
         return results.length === 0 ? null : new PaymentRecord(results[0]);
     }
 
-    static async getAllPayments(): Promise<PaymentEntity[]> {
+    static async getListOfPayments(): Promise<PaymentEntity[]> {
         const [results] = await pool.execute('SELECT * FROM `payments`') as PaymentRecordResult;
-        return results.map(obj=> new PaymentRecord(obj))
+        return results.map(obj => new PaymentRecord(obj))
     }
-    static async sumPaymentsAtOneCategory(idCategory:string) {
-        const [results] = await pool.execute('SELECT SUM(`cost`) FROM `payments` WHERE `idCategory`= :idCategory', {
-            idCategory
-        }) as PaymentRecordResult;
-        return results.length === 0 ? null : new PaymentRecord(results[0]);
-    }
-    //
-    // static async getOneCategoryAtOnePlace(idCategory: string, idPlace: string): Promise<PaymentEntity | null> {
-    //     const [results] = (await pool.execute('SELECT * FROM `payments` WHERE `idCategory` = :idCategory AND `idPlace`= :idPlace', {
-    //         idCategory,
-    //         idPlace
-    //     })) as PaymentRecordResult;
-    //     return results.length === 0 ? null : new PaymentRecord(results[0]);
-    // }
 
-    // static async getAllCategoryAtOnePlace(idPlace: string): Promise<PaymentEntity[] | null> {
-    //     const [results] = await pool.execute('SELECT * FROM `payments` WHERE `idPlace` = :idPlace', {
-    //         idPlace,
-    //     }) as PaymentRecordResult;
-    //     return results.map(result => {
-    //         const {cost, idPlace, currency, idCategory, id, boughtAt} = result;
-    //         return {
-    //             cost,
-    //             idPlace, currency, idCategory, id, boughtAt
-    //         }
-    //     })
-    // }
-    //
-    // static async getOneCategoryAtMonth(month: number, idCategory: string): Promise<PaymentEntity | null> {
-    //     const [results] = (await pool.execute('SELECT SUM(`cost`) FROM `payments` WHERE `idCategory` = :idCategory AND MONTH(`boughtAt`)=:month', {
-    //         month,
-    //         idCategory,
-    //     })) as PaymentRecordResult;
-    //     return results.length === 0 ? null : new PaymentRecord(results[0]);
-    // }
-    //
-    // static async getAllCategoryAtMonth(month: number): Promise<PaymentEntity | null> {
-    //     const [results] = (await pool.execute('SELECT SUM(`cost`) FROM `payments` WHERE  MONTH(`boughtAt`)=:month', {
-    //         month,
-    //     })) as PaymentRecordResult;
-    //     return results.length === 0 ? null : new PaymentRecord(results[0]);
-    // }
-    //
-    // static async getOneCategoryAtYear(year: number, idCategory: string): Promise<PaymentEntity | null> {
-    //     const [results] = (await pool.execute('SELECT SUM(`cost`) FROM `payments` WHERE `idCategory` = :idCategory AND YEAR(`boughtAt`)=:year', {
-    //         year,
-    //         idCategory,
-    //     })) as PaymentRecordResult;
-    //     return results.length === 0 ? null : new PaymentRecord(results[0]);
-    // }
-    //
-    // static async getAllCategoryAtYear(year: number): Promise<PaymentEntity | null> {
-    //     const [results] = (await pool.execute('SELECT SUM(`cost`) FROM `payments` WHERE YEAR(`boughtAt`)=:year', {
-    //         year,
-    //     })) as PaymentRecordResult;
-    //     return results.length === 0 ? null : new PaymentRecord(results[0]);
-    // }
+    static async sumOneCategory(idCategory: string): Promise<number> {
+        const [[{sumOneCategory}]] = await pool.execute('SELECT SUM(`cost`) AS `sumOneCategory` FROM `payments` WHERE `idCategory`= :idCategory',
+            {
+                idCategory,
+            }) as PaymentRecordResult;
+        return sumOneCategory
+    }
+
+    static async sumOneCategoryInOneCountry(idCategory: string, idPlace: string): Promise<number> {
+        const [[{sumOneCategoryInOneCountry}]] = await pool.execute('SELECT SUM(`cost`) AS `sumOneCategoryInOneCountry` FROM `payments` WHERE `idCategory`= :idCategory AND `idPlace`= :idPlace', {
+            idCategory,
+            idPlace,
+        }) as PaymentRecordResult;
+        return sumOneCategoryInOneCountry
+    }
+
+    static async sumAllCategoriesInOneCountry(idPlace: string): Promise<number> {
+        const [[{sumAllCategoriesInOneCountry}]] = await pool.execute('SELECT SUM(`cost`) AS `sumAllCategoriesInOneCountry` FROM `payments` WHERE `idPlace`= :idPlace', {
+            idPlace
+        }) as PaymentRecordResult;
+        return sumAllCategoriesInOneCountry
+    }
+
+    static async sumOneCategoryInOneMonth(idCategory: string, month: number): Promise<number> {
+        const [[{sumOneCategoryInOneMonth}]] = await pool.execute(
+            'SELECT SUM(`cost`) AS `sumOneCategoryInOneMonth` FROM `payments` WHERE `idCategory`= :idCategory AND MONTH(`boughtAt`)= :month',
+            {
+                idCategory,
+                month
+            }) as PaymentRecordResult;
+        return sumOneCategoryInOneMonth
+    }
+
+    static async sumAllCategoriesInOneMonth(month: number): Promise<number> {
+        const [[{sumAllCategoriesInOneMonth}]] = await pool.execute('SELECT SUM(`cost`) AS `sumAllCategoriesInOneMonth` FROM `payments` WHERE MONTH(`boughtAt`)= :month',
+            {
+                month
+            }) as PaymentRecordResult;
+        return sumAllCategoriesInOneMonth
+    }
+
+    static async sumAllCategoriesInOneYear(year: number): Promise<number> {
+        const [[{sumInYear}]] = await pool.execute('SELECT SUM(`cost`) AS `sumInYear` FROM `payments` WHERE YEAR(`boughtAt`)=:year', {
+            year,
+        }) as PaymentRecordResult;
+        return sumInYear
+    }
 }
--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,3 @@
 export * from './payment.entity';
-export * from './place.entity';
+export * from './country.entity';
 export * from './category.entity';
